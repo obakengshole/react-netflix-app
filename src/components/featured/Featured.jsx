0470b8e0e715991ece6ee639281380dd8831901e
obakengshole <shole.obakeng@gmail.com>
2021-11-13 01:05:40 +0200

    Finished Featured component, added options dropdown

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,13 +1,36 @@
 import { InfoOutlined, PlayArrow } from '@material-ui/icons'
 import './featured.scss'
 
-export default function Featured() {
+export default function Featured(type) {
     return (
         <div className="featured">
-            <img src="https://trendsinusa.com/wp-content/uploads/2018/01/Anonymous-hacker-profile-picture.jpg" alt="" />
+            {type && (
+                <div className="category">
+                    <span>{type === "movie" ? "Movies" : "Series"}</span>
+                    <select name="genre" id="genre">
+                        <option value="">Genre</option>
+                        <option value="comedy">Comedy</option>
+                        <option value="crime">Crime</option>
+                        <option value="fantasy">Fantasy</option>
+                        <option value="historical">Historical</option>
+                        <option value="action">Action</option>
+                        <option value="romance">Romance</option>
+                        <option value="comedy">Comedy</option>
+                        <option value="drama">Drama</option>
+                        <option value="horror">Horror</option>
+                        <option value="fantasy">Fantasy</option>
+                        <option value="sci-fi">Sci-fi</option>
+                        <option value="thriller">Thriller</option>
+                        <option value="western">Western</option>
+                        <option value="animation">Animation</option>
+                        <option value="documentary">Documentary</option>
+                    </select>
+                </div>
+            )}
+            <img src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" />
             
             <div className="info">
-                <img src="https://trendsinusa.com/wp-content/uploads/2018/01/Anonymous-hacker-profile-picture.jpg" alt="" />
+                <img src="https://occ-0-1432-1433.1.nflxso.net/dnm/api/v6/LmEnxtiAuzezXBjYXPuDgfZ4zZQ/AAAABUZdeG1DrMstq-YKHZ-dA-cx2uQN_YbCYx7RABDk0y7F8ZK6nzgCz4bp5qJVgMizPbVpIvXrd4xMBQAuNe0xmuW2WjoeGMDn1cFO.webp?r=df1" alt="" />
                 <span className="desc">
                     Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos illum excepturi explicabo debitis, tempora exercitationem nulla voluptatibus repudiandae rem laborum accusamus quam, aperiam commodi porro expedita. Velit dolore quaerat assumenda!
                 </span>
--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -6,13 +6,8 @@ import Featured from '../components/featured/Featured'
 const Home = () => {
     return (
         <div className="home">
-            <Navbar />
-            <img
-            width="100%"
-            src="https://trendsinusa.com/wp-content/uploads/2018/01/Anonymous-hacker-profile-picture.jpg"
-            alt=""
-          />
-          <Featured/>
+          <Navbar />
+          <Featured type="series"/>
         </div>
     )
 }
